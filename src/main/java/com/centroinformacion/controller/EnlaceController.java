482e5bea7f0c5074f54cdb84eb0388c107967bcc
SebastianGarroDelRio <sebastiangarrodelrio@gmail.com>
2023-12-03 21:05:18 -0500

    ca

--- src/main/java/com/centroinformacion/controller/EnlaceController.java
+++ src/main/java/com/centroinformacion/controller/EnlaceController.java
@@ -1,6 +1,7 @@
 package com.centroinformacion.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EnlaceController {
 	
 	@RequestMapping("/Inicio")
-	public String Inicio() {
+	public String Inicio(Model model) {
 		return "Inicio";
 	}
 	
