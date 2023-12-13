d80c001dcf44d6d069ced7467a369459785fe60f
SebastianGarroDelRio <sebastiangarrodelrio@gmail.com>
2023-12-13 18:04:12 -0500

    cambios

--- src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
+++ src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
@@ -3,7 +3,7 @@ package com.centroinformacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.centroinformacion")
 public class ProjectLinertecBackendApplication {
 
 	public static void main(String[] args) {
