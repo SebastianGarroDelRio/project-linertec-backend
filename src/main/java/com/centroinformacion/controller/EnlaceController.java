548118cc6498a0256da932984f18c58fe6493286
SebastianGarroDelRio <sebastiangarrodelrio@gmail.com>
2023-12-13 16:04:07 -0500

    a

--- src/main/java/com/centroinformacion/controller/EnlaceController.java
+++ src/main/java/com/centroinformacion/controller/EnlaceController.java
@@ -1,17 +1,18 @@
 package com.centroinformacion.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnlaceController {
 	
-	@GetMapping("/verLogin")
+	@RequestMapping("/verLogin")
 	public String verLogin() {
 		return "intranetLogin";
 	}
 
-	@GetMapping("/verIntranetHome")
+	@RequestMapping("/verIntranetHome")
 	public String verIntranetHome() {
 		return "intranetHome";
 	}
