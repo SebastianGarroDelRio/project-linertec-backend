be9216bec034d692645f29896bb30565e5499a32
SebastianGarroDelRio <sebastiangarrodelrio@gmail.com>
2023-12-13 17:09:01 -0500

    a

--- src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
+++ src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
@@ -3,9 +3,7 @@ package com.centroinformacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.centroinformacion.controller.EnlaceController;
-
-@SpringBootApplication(scanBasePackageClasses = EnlaceController.class)
+@SpringBootApplication
 public class ProjectLinertecBackendApplication {
 
 	public static void main(String[] args) {
