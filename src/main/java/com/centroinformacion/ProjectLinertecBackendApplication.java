38cc6eeeca7af989c8e1e9e6f3bc13b5436dcc62
SebastianGarroDelRio <sebastiangarrodelrio@gmail.com>
2023-12-13 16:56:45 -0500

    ajustes

--- src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
+++ src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
@@ -3,7 +3,9 @@ package com.centroinformacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.centroinformacion.controller")
+import com.centroinformacion.controller.EnlaceController;
+
+@SpringBootApplication(scanBasePackageClasses = EnlaceController.class)
 public class ProjectLinertecBackendApplication {
 
 	public static void main(String[] args) {
