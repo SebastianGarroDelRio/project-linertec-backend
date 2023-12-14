92b7cb2741402aa8b63f435187df8a4f166e7c2e
SebastianGarroDelRio <sebastiangarrodelrio@gmail.com>
2023-12-13 20:25:30 -0500

    ajustes

--- src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
+++ src/main/java/com/centroinformacion/ProjectLinertecBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.centroinformacion.controller")
+@SpringBootApplication(scanBasePackages = "com.centroinformacion")
 public class ProjectLinertecBackendApplication extends SpringBootServletInitializer {
 	
 	@Override
